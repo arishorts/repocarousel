bcf25df330cd83fa66edfe48589ce28f339d9c9e
arishorts <arieljschwartz@gmail.com>
2023-01-03 16:19:13 -0700

    Added carousel effect js by css selector manipulation Bug: issues with local/global variables not communicating

--- index.js
+++ index.js
@@ -8,18 +8,67 @@ function generateRepoAnchors() {
     .then((result) => result.json())
     .then((data) => {
       const myAPIObjects = Object.assign(data);
-      const bodyEl = document.querySelector("body");
+      const divEl = document.getElementById("carousel");
       for (let key in myAPIObjects) {
         let urlnames = `https://arishorts.github.io/${myAPIObjects[key].name}/`;
         let reponames = myAPIObjects[key].name;
         myRepoArray.push({ urlname: urlnames, reponame: reponames });
         const aEl = document.createElement("a");
-        const divEl = document.createElement("div");
-        aEl.innerHTML = reponames;
+        const spanEl = document.createElement("span");
+        aEl.id = `carousel__item ${Number(key) + 1}`;
         aEl.href = urlnames;
-        bodyEl.appendChild(divEl);
+        spanEl.id = "carousel__item--text";
+        spanEl.innerHTML = reponames;
         divEl.appendChild(aEl);
+        aEl.appendChild(spanEl);
       }
+      let firstaEl = document.getElementById("carousel__item 1");
+      firstaEl.id = "carousel__item carousel__item--visible";
     });
 }
 generateRepoAnchors();
+
+let slidePosition = 0;
+const slides = document.getElementsByClassName("carousel__item");
+const totalSlides = slides.length;
+console.log(totalSlides);
+
+document
+  .getElementById("carousel__button--next")
+  .addEventListener("click", function () {
+    moveToNextSlide();
+  });
+document
+  .getElementById("carousel__button--prev")
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel__item--visible");
+    slide.classList.add("carousel__item--hidden");
+  }
+
+  slides[slidePosition].classList.add("carousel__item--visible");
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
